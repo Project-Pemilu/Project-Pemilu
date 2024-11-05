9ff6f67ac9b8966c0fbbdf0e783e087ae21c7113
Joses-Steven-Tarigan <steventarigan82@gmail.com>
2024-11-05 21:44:47 +0700

    Fix: voteController

--- server/controllers/voteController.js
+++ server/controllers/voteController.js
@@ -4,7 +4,7 @@ const { User } = require("../models");
 exports.getCandidates = async (req, res, next) => {
   try {
     let candidates = await Candidate.findAll();
-    res.status(200).json({ candidates });
+    res.status(200).json(candidates);
   } catch (error) {
     console.log(error, "<<<<error getCandidates");
     next(error);
@@ -13,32 +13,24 @@ exports.getCandidates = async (req, res, next) => {
 
 exports.patchVote = async (req, res, next) => {
   const { id } = req.params;
-  const { CandidateId } = req.body;
-
+  const { CandidateId } = req.body
   try {
     const user = await User.findByPk(id);
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      throw ({name: "NotFound", message: "User Not Found"});
     }
 
     if (user.CandidateId) {
-      return res.status(400).json({ message: "User has already voted" });
+        throw ({name: "BadRequest", message: "User Already Vote"});
     }
 
-    const candidate = await Candidate.findByPk(CandidateId);
-    if (!candidate) {
-      return res.status(404).json({ message: "Candidate not found" });
-    }
 
     user.CandidateId = CandidateId;
     await user.save();
 
-    await candidate.increment("totalVote");
 
     return res.status(200).json({
       message: "Vote successfully cast",
-      user,
-      candidate,
     });
   } catch (error) {
     console.log(error, "<<<<error patchVote");
