50aba818143cb186943dbc299444c7bc6ad0501a
Prima <widianiprima@gmail.com>
2024-11-05 21:13:23 +0700

    feat(client): login, done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,23 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <div>Hello world!</div>,
-  },
+	{
+		path: "/",
+		element: <div>Hello world!</div>,
+	},
+	{
+		path: "/login",
+		element: <LoginPage />,
+	},
 ]);
 
 function App() {
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+	return (
+		<>
+			<RouterProvider router={router} />
+		</>
+	);
 }
 
 export default App;
