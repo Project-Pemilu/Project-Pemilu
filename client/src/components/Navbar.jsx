16e15aa76d8b69240c9b69387876f5588c870183
Prima <widianiprima@gmail.com>
2024-11-05 22:28:48 +0700

    feat(client): edit logout and navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,13 +7,13 @@ export default function Navbar() {
 		navigate("/login");
 	};
 	return (
-		<nav className="bg-white border-gray-200 dark:bg-gray-900">
+		<nav className="bg-white">
 			<div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 				<Link
 					to="/"
 					className="flex items-center space-x-3 rtl:space-x-reverse">
 					<img src="#" className="h-8" alt="Hacktiv-pemilu" />
-					<span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+					<span className="self-center text-2xl font-semibold whitespace-nowrap">
 						Hacktiv Pemilu
 					</span>
 				</Link>
@@ -21,7 +21,7 @@ export default function Navbar() {
 					<button
 						type="button"
 						onClick={handleLogout}
-						className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+						className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center">
 						Logout
 					</button>
 				</div>
