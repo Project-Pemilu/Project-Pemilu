f410a30f5de37fa8713bd8f84a1be8c460341793
fathanabds <abdulshodiqfathan@gmail.com>
2024-11-06 13:23:24 +0700

    chore: add logo image

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -28,7 +28,7 @@ export default function LoginPage() {
     <>
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img className="mx-auto h-10 w-auto" src="#" alt="Hacktiv-voting" />
+          <img className="mx-auto h-40 w-auto" src="vote.png" alt="Hacktiv-voting" />
           <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Sign in</h2>
         </div>
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
